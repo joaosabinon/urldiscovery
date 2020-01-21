525908ce3d7f2b5da7ff118449639cd69e6e43f5
João Sabino <joao.nsabino@gmail.com>
2020-01-21 15:46:33 -0300

    Improved save option

--- main.py
+++ main.py
@@ -2,14 +2,37 @@
 import webbrowser
 import pyscreenshot as ImageGrab
 import time
+import datetime
+import hashlib
+import re
 
 url = "https://google.com.br"
 
-if __name__ == "__main__":
+# take the archive name
+def archiveName(string):
+    cleanString = re.sub('[^A-Za-z0-9]+', '', string)
+    stringCipher = hashlib.md5(cleanString.encode())
+    return str(stringCipher.hexdigest())
 
-    web = webbrowser.open(url, new=0, autoraise=True)
-    time.sleep(4)
-    im = ImageGrab.grab(backend='gnome-screenshot')  # X1,Y1,X2,Y2
-    im.show()
+def discovery():
+    if __name__ == "__main__":
+        #open the site
+        webbrowser.open(url, new=0, autoraise=True)
+        time.sleep(4)
 
-#webbrowser.cle()
+        #take the screenshot
+        im = ImageGrab.grab(backend='gnome-screenshot')  # X1,Y1,X2,Y2
+
+        #save the screenshot
+        now = datetime.datetime.now()
+        archive = archiveName(str(now))
+        path = '/tmp/'+archive+'.png'
+        im.save(path)
+
+        #return the path
+        return str(path)
+
+discovery()
+
+
+
